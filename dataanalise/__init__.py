969cbd87ba878179b4adc4bff37e434e886f277d
renegadelha <renegadelha@gmail.com>
2024-02-21 21:49:52 -0300

    aula2102pos

--- dataanalise/__init__.py
+++ dataanalise/__init__.py
@@ -1,17 +1,25 @@
 import pandas as pd
+import plotly.express as px
 
 def analisar():
     dados = pd.read_html('https://pt.wikipedia.org/wiki/Lista_de_munic%C3%ADpios_do_Brasil_por_taxa_de_homic%C3%ADdios')
-
     mais250 = dados[0]
     menores = dados[1]
-
     print(dados[0])
 
 def analisar2():
     dados = pd.read_csv('dadosindicadoresPB3.csv')
-    dados.drop(columns=['code'],inplace=True)
+    #excluir colunas
+    dados.drop(columns=['code','cvp'], inplace=True)
     return dados
+
+def exibirmapacorrelacoes(data):
+    data.drop(columns=['municipio'], inplace=True)
+
+    fig = px.imshow(data.corr())
+    return fig
+
+
 
 '''
 ['Municípios', 'Gentílico',
--- main.py
+++ main.py
@@ -5,25 +5,26 @@ import plotly.express as px
 
 app = Flask(__name__)
 
-@app.route('/cadastrar', methods=['POST'])
+@app.route('/login', methods=['POST'])
 def cadastrar_usuario():
     nome = str(request.form.get('nome'))
     senha = str(request.form.get('senha'))
 
     if dao.verificarlogin(nome, senha):
         dados = da.analisar2()
-        fig = px.scatter(dados, x='rendapercapita', y='idebanosfinais', hover_data=['municipio'])
-        return render_template('verificado.html', login=nome, plot=fig.to_html())
+        dados.drop(dados.sort_values(by=['cvli'], ascending=False).head(3).index, inplace=True)
+        dados.drop(dados.sort_values(by=['rendapercapita'], ascending=False).head(10).index, inplace=True)
+        dados.drop(dados.sort_values(by=['rendapercapita'], ascending=True).head(2).index, inplace=True)
+        fig = px.scatter(dados, x='rendapercapita', y='cvli', hover_data=['municipio'])
+        fig2 = da.exibirmapacorrelacoes(da.analisar2())
+
+        return render_template('verificado.html', login=nome, plot=fig.to_html(), mapa=fig2.to_html())
     else:
         return render_template('index.html')
 
 
 @app.route('/')
 def motormanda():
-    return render_template('index.html')
-
-@app.route('/home')
-def home():
     return render_template('index2.html')
 
 if __name__ == '__main__':
