10d01bd29d714647790f7b7e962a8202bd9d144a
renegadelha <renegadelha@gmail.com>
2024-03-06 19:01:57 -0300

    aula0603

--- dao/__init__.py
+++ dao/__init__.py
@@ -1,5 +1,50 @@
+import psycopg2
 def verificarlogin(nome, senha):
-    if nome == 'rene@rene.com' and senha == '123':
+    conexao = conectardb()
+    cur = conexao.cursor()
+    cur.execute(f"SELECT count(*) FROM usuarios WHERE login = '{nome}' AND senha = '{senha}'")
+    recset = cur.fetchall()
+    conexao.close()
+    if recset[0][0] == 1:
         return True
     else:
         return False
+
+
+
+def conectardb():
+    con = psycopg2.connect(
+        host='localhost',
+        database='aplicacaoteste',
+        user='postgres',
+        password='12345'
+    )
+
+    return con
+
+
+def inserirDB(login, senha, conexao):
+    cur = conexao.cursor()
+    exito = False
+    try:
+        sql = f"INSERT INTO usuarios (login, senha) VALUES ('{login}', '{senha}' )"
+        cur.execute(sql)
+    except psycopg2.IntegrityError:
+        conexao.rollback()
+        exito = False
+    else:
+        conexao.commit()
+        exito = True
+
+    conexao.close()
+    return exito
+
+
+
+def listarUsuarios(conexao):
+    cur = conexao.cursor()
+    cur.execute('select * from usuarios')
+    recset = cur.fetchall()
+    conexao.close()
+
+    return recset
